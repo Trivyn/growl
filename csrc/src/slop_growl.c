fc26de41e44bce346f490b7b90337a1067c6360f
James Adam <jcadam@startmail.com>
2026-06-29 06:32:45 -0800

    update slop-rdf

--- csrc/src/slop_growl.c
+++ csrc/src/slop_growl.c
@@ -12,21 +12,21 @@ slop_list_rdf_Term growl_get_same_as(slop_arena* arena, index_IndexedGraph g, rd
 int64_t growl_get_inferred_count(types_ReasonerResult result);
 
 rdf_Term growl_remap_blank_term(slop_arena* arena, rdf_Term t, int64_t offset) {
-    __auto_type _mv_308 = t;
-    switch (_mv_308.tag) {
+    __auto_type _mv_307 = t;
+    switch (_mv_307.tag) {
         case rdf_Term_term_blank:
         {
-            __auto_type b = _mv_308.data.term_blank;
+            __auto_type b = _mv_307.data.term_blank;
             return rdf_make_blank(arena, (b.id + offset));
         }
         case rdf_Term_term_iri:
         {
-            __auto_type _ = _mv_308.data.term_iri;
+            __auto_type _ = _mv_307.data.term_iri;
             return t;
         }
         case rdf_Term_term_literal:
         {
-            __auto_type _ = _mv_308.data.term_literal;
+            __auto_type _ = _mv_307.data.term_literal;
             return t;
         }
     }
@@ -40,11 +40,11 @@ int64_t growl_max_blank_id_in_graph(index_IndexedGraph ig) {
             __auto_type _coll = triples;
             for (size_t _i = 0; _i < _coll.len; _i++) {
                 __auto_type t = _coll.data[_i];
-                __auto_type _mv_309 = t.subject;
-                switch (_mv_309.tag) {
+                __auto_type _mv_308 = t.subject;
+                switch (_mv_308.tag) {
                     case rdf_Term_term_blank:
                     {
-                        __auto_type b = _mv_309.data.term_blank;
+                        __auto_type b = _mv_308.data.term_blank;
                         if (b.id > max_id) {
                             max_id = b.id;
                         }
@@ -54,11 +54,11 @@ int64_t growl_max_blank_id_in_graph(index_IndexedGraph ig) {
                         break;
                     }
                 }
-                __auto_type _mv_310 = t.object;
-                switch (_mv_310.tag) {
+                __auto_type _mv_309 = t.object;
+                switch (_mv_309.tag) {
                     case rdf_Term_term_blank:
                     {
-                        __auto_type b = _mv_310.data.term_blank;
+                        __auto_type b = _mv_309.data.term_blank;
                         if (b.id > max_id) {
                             max_id = b.id;
                         }
@@ -109,21 +109,21 @@ types_ReasonerResult growl_reason_with_config(slop_arena* arena, index_IndexedGr
 
 uint8_t growl_is_consistent(slop_arena* arena, index_IndexedGraph input) {
     SLOP_PRE(((rdf_indexed_graph_size(input) >= 0)), "(>= (indexed-graph-size input) 0)");
-    __auto_type _mv_311 = growl_reason(arena, input);
-    switch (_mv_311.tag) {
+    __auto_type _mv_310 = growl_reason(arena, input);
+    switch (_mv_310.tag) {
         case types_ReasonerResult_reason_success:
         {
-            __auto_type _ = _mv_311.data.reason_success;
+            __auto_type _ = _mv_310.data.reason_success;
             return 1;
         }
         case types_ReasonerResult_reason_inconsistent:
         {
-            __auto_type _ = _mv_311.data.reason_inconsistent;
+            __auto_type _ = _mv_310.data.reason_inconsistent;
             return 0;
         }
         case types_ReasonerResult_reason_cancelled:
         {
-            __auto_type _ = _mv_311.data.reason_cancelled;
+            __auto_type _ = _mv_310.data.reason_cancelled;
             return 1;
         }
     }
@@ -182,21 +182,21 @@ slop_list_rdf_Term growl_get_same_as(slop_arena* arena, index_IndexedGraph g, rd
 
 int64_t growl_get_inferred_count(types_ReasonerResult result) {
     int64_t _retval = {0};
-    __auto_type _mv_312 = result;
-    switch (_mv_312.tag) {
+    __auto_type _mv_311 = result;
+    switch (_mv_311.tag) {
         case types_ReasonerResult_reason_success:
         {
-            __auto_type s = _mv_312.data.reason_success;
+            __auto_type s = _mv_311.data.reason_success;
             return s.inferred_count;
         }
         case types_ReasonerResult_reason_inconsistent:
         {
-            __auto_type _ = _mv_312.data.reason_inconsistent;
+            __auto_type _ = _mv_311.data.reason_inconsistent;
             return 0;
         }
         case types_ReasonerResult_reason_cancelled:
         {
-            __auto_type s = _mv_312.data.reason_cancelled;
+            __auto_type s = _mv_311.data.reason_cancelled;
             return s.inferred_count;
         }
     }
--- csrc/src/slop_ttl.h
+++ csrc/src/slop_ttl.h
@@ -18,6 +18,7 @@ typedef struct ttl_StringResult ttl_StringResult;
 typedef struct ttl_EscapeResult ttl_EscapeResult;
 typedef struct ttl_TripleResult ttl_TripleResult;
 typedef struct ttl_TriplesResult ttl_TriplesResult;
+typedef struct ttl_GraphTriplesResult ttl_GraphTriplesResult;
 typedef struct ttl_TermTriplesResult ttl_TermTriplesResult;
 typedef struct ttl_TtlFileError ttl_TtlFileError;
 typedef struct ttl_TtlParseContext ttl_TtlParseContext;
@@ -181,6 +182,17 @@ typedef struct ttl_TriplesResult ttl_TriplesResult;
 SLOP_OPTION_DEFINE(ttl_TriplesResult, slop_option_ttl_TriplesResult)
 #endif
 
+struct ttl_GraphTriplesResult {
+    rdf_Graph graph;
+    ttl_TtlParseContext ctx;
+};
+typedef struct ttl_GraphTriplesResult ttl_GraphTriplesResult;
+
+#ifndef SLOP_OPTION_TTL_GRAPHTRIPLESRESULT_DEFINED
+#define SLOP_OPTION_TTL_GRAPHTRIPLESRESULT_DEFINED
+SLOP_OPTION_DEFINE(ttl_GraphTriplesResult, slop_option_ttl_GraphTriplesResult)
+#endif
+
 struct ttl_TermTriplesResult {
     rdf_Term term;
     slop_list_rdf_Triple extra_triples;
@@ -198,14 +210,14 @@ SLOP_OPTION_DEFINE(ttl_TermTriplesResult, slop_option_ttl_TermTriplesResult)
 typedef struct { bool is_ok; union { ttl_TermResult ok; common_ParseError err; } data; } slop_result_ttl_TermResult_common_ParseError;
 #endif
 
-#ifndef SLOP_RESULT_TTL_TERMTRIPLESRESULT_COMMON_PARSEERROR_DEFINED
-#define SLOP_RESULT_TTL_TERMTRIPLESRESULT_COMMON_PARSEERROR_DEFINED
-typedef struct { bool is_ok; union { ttl_TermTriplesResult ok; common_ParseError err; } data; } slop_result_ttl_TermTriplesResult_common_ParseError;
-#endif
-
 #ifndef SLOP_RESULT_TTL_STRINGRESULT_COMMON_PARSEERROR_DEFINED
 #define SLOP_RESULT_TTL_STRINGRESULT_COMMON_PARSEERROR_DEFINED
 typedef struct { bool is_ok; union { ttl_StringResult ok; common_ParseError err; } data; } slop_result_ttl_StringResult_common_ParseError;
+#endif
+
+#ifndef SLOP_RESULT_TTL_TERMTRIPLESRESULT_COMMON_PARSEERROR_DEFINED
+#define SLOP_RESULT_TTL_TERMTRIPLESRESULT_COMMON_PARSEERROR_DEFINED
+typedef struct { bool is_ok; union { ttl_TermTriplesResult ok; common_ParseError err; } data; } slop_result_ttl_TermTriplesResult_common_ParseError;
 #endif
 
 #ifndef SLOP_RESULT_TTL_ESCAPERESULT_COMMON_PARSEERROR_DEFINED
@@ -233,6 +245,11 @@ typedef struct { bool is_ok; union { ttl_TripleResult ok; common_ParseError err;
 typedef struct { bool is_ok; union { ttl_TriplesResult ok; common_ParseError err; } data; } slop_result_ttl_TriplesResult_common_ParseError;
 #endif
 
+#ifndef SLOP_RESULT_TTL_GRAPHTRIPLESRESULT_COMMON_PARSEERROR_DEFINED
+#define SLOP_RESULT_TTL_GRAPHTRIPLESRESULT_COMMON_PARSEERROR_DEFINED
+typedef struct { bool is_ok; union { ttl_GraphTriplesResult ok; common_ParseError err; } data; } slop_result_ttl_GraphTriplesResult_common_ParseError;
+#endif
+
 #ifndef SLOP_RESULT_RDF_GRAPH_COMMON_PARSEERROR_DEFINED
 #define SLOP_RESULT_RDF_GRAPH_COMMON_PARSEERROR_DEFINED
 typedef struct { bool is_ok; union { rdf_Graph ok; common_ParseError err; } data; } slop_result_rdf_Graph_common_ParseError;
@@ -253,9 +270,12 @@ ttl_TtlParseContext ttl_ctx_with_state(ttl_TtlParseContext ctx, common_ParseStat
 slop_string ttl_iri_string(rdf_Term t);
 slop_list_rdf_Triple ttl_list_push_triples(slop_arena* arena, slop_list_rdf_Triple dst, slop_list_rdf_Triple src);
 rdf_Graph ttl_graph_add_all(slop_arena* arena, rdf_Graph g, slop_list_rdf_Triple src);
+slop_option_string ttl_some_string(slop_string value);
 slop_result_ttl_TermResult_common_ParseError ttl_parse_iri_ref(slop_arena* arena, ttl_TtlParseContext ctx);
+slop_result_ttl_StringResult_common_ParseError ttl_parse_iri_ref_string(slop_arena* arena, ttl_TtlParseContext ctx);
 common_ParseWhileResult ttl_parse_pn_local(slop_arena* arena, common_ParseState state);
 slop_result_ttl_TermResult_common_ParseError ttl_parse_prefixed_name(slop_arena* arena, ttl_TtlParseContext ctx);
+slop_result_ttl_StringResult_common_ParseError ttl_parse_prefixed_name_string(slop_arena* arena, ttl_TtlParseContext ctx);
 slop_result_ttl_TermTriplesResult_common_ParseError ttl_parse_blank_node_extended(slop_arena* arena, ttl_TtlParseContext ctx);
 slop_result_ttl_TermResult_common_ParseError ttl_parse_blank_node(slop_arena* arena, ttl_TtlParseContext ctx);
 slop_result_ttl_StringResult_common_ParseError ttl_parse_string_literal(slop_arena* arena, ttl_TtlParseContext ctx);
@@ -274,6 +294,8 @@ slop_result_ttl_TripleResult_common_ParseError ttl_parse_triple(slop_arena* aren
 slop_result_ttl_TriplesResult_common_ParseError ttl_parse_predicate_object_list(slop_arena* arena, ttl_TtlParseContext ctx, rdf_Term subject);
 slop_result_ttl_TriplesResult_common_ParseError ttl_parse_annotation(slop_arena* arena, ttl_TtlParseContext ctx, rdf_Triple base_triple);
 slop_result_ttl_TriplesResult_common_ParseError ttl_parse_object_list(slop_arena* arena, ttl_TtlParseContext ctx, rdf_Term subject, rdf_Term predicate);
+slop_result_ttl_GraphTriplesResult_common_ParseError ttl_parse_object_list_into_graph(slop_arena* arena, ttl_TtlParseContext ctx, rdf_Graph g, rdf_Term subject, rdf_Term predicate);
+slop_result_ttl_GraphTriplesResult_common_ParseError ttl_parse_predicate_object_list_into_graph(slop_arena* arena, ttl_TtlParseContext ctx, rdf_Graph g, rdf_Term subject);
 slop_result_rdf_Graph_common_ParseError ttl_parse_ttl_string(slop_arena* arena, slop_string input);
 slop_result_rdf_Graph_ttl_TtlFileError ttl_parse_ttl_file(slop_arena* arena, slop_string path);
 uint8_t ttl_is_pn_chars_base(uint8_t c);
@@ -327,6 +349,11 @@ SLOP_OPTION_DEFINE(rdf_Triple, slop_option_rdf_Triple)
 #ifndef SLOP_OPTION_TTL_TRIPLESRESULT_DEFINED
 #define SLOP_OPTION_TTL_TRIPLESRESULT_DEFINED
 SLOP_OPTION_DEFINE(ttl_TriplesResult, slop_option_ttl_TriplesResult)
+#endif
+
+#ifndef SLOP_OPTION_TTL_GRAPHTRIPLESRESULT_DEFINED
+#define SLOP_OPTION_TTL_GRAPHTRIPLESRESULT_DEFINED
+SLOP_OPTION_DEFINE(ttl_GraphTriplesResult, slop_option_ttl_GraphTriplesResult)
 #endif
 
 #ifndef SLOP_OPTION_TTL_TERMTRIPLESRESULT_DEFINED
